7e9ba02e2cec8882d5192d5c8de8d7046160a792
Kristian Toustrup Langborg-Hansen <ktlh@smartlearning.dk>
2021-11-18 13:18:29 +0100

    Add project files.

---
+++ Kortspil-mac/MainWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kortspil_mac
+{
+    class MainWindow
+    {
+        public string FindBillede(int kortnummer)
+        {
+            string resultat = "2-Spar.jpg";
+
+            // Skriv din løsning her...
+
+            return resultat;
+        }
+    }
+}
---
+++ Kortspil-mac/Program.cs
@@ -0,0 +1,9 @@
+﻿using Kortspil_mac;
+using System;
+
+MainWindow window = new();
+Console.WriteLine("Indtast nummeret på det kort, der skal vises (1-52)");
+int kortnummer = Convert.ToInt32(Console.ReadLine());
+
+string filnavn = window.FindBillede(kortnummer);
+Console.WriteLine($"Billedet af kortet ligger i filen Billeder/{filnavn}");
